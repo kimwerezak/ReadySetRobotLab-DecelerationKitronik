3e0a2eda56c7c7f2dde5f72df86d195e1917ebb0
Kimberley Werezak <122053493+kimwerezak@users.noreply.github.com>
2025-04-10 14:38:37 -0600

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
+#This will move the position of the servo to create tick mark each second
 def move_servo(position_1, position_2, time_1, time_2): 
     pins.servo_write_pin(AnalogPin.P15, position_1)
     basic.pause(time_1)
     pins.servo_write_pin(AnalogPin.P15, position_2)
     basic.pause(time_2)
-        
-def deaccelerate_movement(speed, delay, deaccelerations): 
+
+#This will decelerate the robot and leave a tick mark with each deceleration. 
+def decelerate_movement(speed, decelerations): 
     Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, speed)
-    for index in range(deaccelerations):
-        move_servo(80,135,50,950)
-        basic.pause(delay)
+    for index in range(decelerations):
+        move_servo(0, 180, 200, 800)
         speed -= 5
         Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, speed)
     Kitronik_Move_Motor.stop()
 
+#The main function will decelerate the robot, starting at speed 40 for 6 decelerations,
+#leaving a tick with each deceleration.
 def main():
-    deaccelerate_movement(55,500,9)
+    decelerate_movement(40, 6)
 
 input.on_logo_event(TouchButtonEvent.PRESSED, main)
 
--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+// This will move the position of the servo to create tick mark each second
 function move_servo(position_1: number, position_2: number, time_1: number, time_2: number) {
     pins.servoWritePin(AnalogPin.P15, position_1)
     basic.pause(time_1)
@@ -5,17 +6,19 @@ function move_servo(position_1: number, position_2: number, time_1: number, time
     basic.pause(time_2)
 }
 
-function deaccelerate_movement(speed: number, delay: number, deaccelerations: number) {
+// This will decelerate the robot and leave a tick mark with each deceleration. 
+function decelerate_movement(speed: number, decelerations: number) {
     Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
-    for (let index = 0; index < deaccelerations; index++) {
-        move_servo(80, 135, 50, 950)
-        basic.pause(delay)
+    for (let index = 0; index < decelerations; index++) {
+        move_servo(0, 180, 200, 800)
         speed -= 5
         Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
     }
     Kitronik_Move_Motor.stop()
 }
 
+// The main function will decelerate the robot, starting at speed 40 for 6 decelerations,
+// leaving a tick with each deceleration.
 input.onLogoEvent(TouchButtonEvent.Pressed, function main() {
-    deaccelerate_movement(55, 500, 9)
+    decelerate_movement(40, 6)
 })
